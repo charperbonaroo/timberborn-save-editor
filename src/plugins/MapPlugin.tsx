4547cfe6739b7af77a655d35a7d242e47453e024
Toby Hinloopen <hinlomedia@gmail.com>
2021-09-12 17:24:34 +0200

    Slightly increase performance

--- src/plugins/MapPlugin.tsx
+++ src/plugins/MapPlugin.tsx
@@ -6,34 +6,69 @@ import lodash from "lodash";
 import { MapControls } from "@react-three/drei";
 import './MapPlugin.scss';
 import { Navbar } from "../Navbar";
-import { BoxGeometry, ConeBufferGeometry, Mesh, MeshStandardMaterial, PlaneBufferGeometry } from "three";
+import { BoxBufferGeometry, BufferGeometry, ConeBufferGeometry, Mesh, MeshStandardMaterial, PlaneBufferGeometry } from "three";
 import { EffectComposer, SSAO as _SSAO } from "@react-three/postprocessing";
 
 const SSAO = _SSAO as any;
 const { BlendFunction } = require("postprocessing") as any;
 const BufferGeometryUtils = require('three/examples/jsm/utils/BufferGeometryUtils.js') as any;
 
-export const MapPlugin: IEditorPlugin<DemoSave, DemoSave> = {
+interface State {
+  saveData: DemoSave;
+  mapData: MapData;
+}
+
+interface MapData {
+  i2x: (i: number) => number,
+  i2y: (i: number) => number,
+  i2xy: (i: number) => [number, number],
+  i2xyz: (i: number, y: number) => [number, number, number],
+  heightMap: Uint8Array;
+  waterDepthMap: Float32Array;
+  moistureMap: Float32Array;
+  mapSizeX: number;
+  mapSizeY: number;
+}
+
+function readMapData(saveData: DemoSave) {
+  const {Singletons} = saveData;
+  const mapSizeX = Singletons.MapSize.Size.X;
+  const mapSizeY = Singletons.MapSize.Size.Y;
+
+  return {
+    i2x: (index: number) => index % mapSizeY,
+    i2y: (index: number) => Math.floor(index / mapSizeY),
+    i2xy: (index: number) => [index % mapSizeY, Math.floor(index / mapSizeY)] as [number, number],
+    i2xyz: (index: number, y: number) => [index % mapSizeY, y, Math.floor(index / mapSizeY)] as [number, number, number],
+    mapSizeX,
+    mapSizeY,
+    heightMap: Uint8Array.from(Singletons.TerrainMap.Heights.Array.split(" ").map(_ => parseInt(_, 10))),
+    moistureMap: Float32Array.from(Singletons.SoilMoistureSimulator.MoistureLevels.Array.split(" ").map(_ => parseFloat(_))),
+    waterDepthMap: Float32Array.from(Singletons.WaterMap.WaterDepths.Array.split(" ").map(_ => parseFloat(_))),
+  }
+}
+
+
+export const MapPlugin: IEditorPlugin<State, State> = {
   id: "MapPlugin",
   name: "Map",
   position: 2,
   group: "General",
   enabled: true,
 
-  read: (saveData) => saveData,
-  write: (saveData, data) => data,
+  read: (saveData) => ({
+    mapData: readMapData(saveData),
+    saveData
+  }),
+
+  write: (_saveData, { saveData }) => saveData,
 
   Preview: ({ saveData }) => <div>
     A 3D Map
   </div>,
 
-  Editor: ({ initialData, onSubmit, onClose }) => {
-
-    // X: Left/Right
-    // Y: Up (+), Down (-)
-    // Z: Front (+), Far (-)
-
-    const {Singletons} = initialData;
+  Editor: ({ initialData: state, onSubmit, onClose }) => {
+    const {Singletons} = state.saveData;
     const mapSizeX = Singletons.MapSize.Size.X;
     const mapSizeY = Singletons.MapSize.Size.Y;
 
@@ -54,11 +89,11 @@ export const MapPlugin: IEditorPlugin<DemoSave, DemoSave> = {
         <directionalLight position={[10, 10, 10]} intensity={0.4} />
         <group scale={[1, 1, -1]}>
           <group position={[mapSizeX/-2, 0, mapSizeY/-2]}>
-            <SlowBoxesHeightMap initialData={initialData} />
-            <SlowBoxesWaterMap initialData={initialData} />
-            <TreesMap initialData={initialData} />
-            <PathsMap initialData={initialData} />
-            <BeaversMap initialData={initialData} />
+            <SlowBoxesHeightMap {...state} />
+            <SlowBoxesWaterMap {...state} />
+            <TreesMap {...state} />
+            <PathsMap {...state} />
+            <BeaversMap {...state} />
           </group>
         </group>
         <MapControls />
@@ -67,9 +102,9 @@ export const MapPlugin: IEditorPlugin<DemoSave, DemoSave> = {
   }
 }
 
-function BeaversMap({ initialData }: { initialData: DemoSave }) {
-  const beavers = useMemo(() => lodash(initialData.Entities)
-    .filter((_) => ["BeaverAdult", "BeaverChild"].includes(_.TemplateName)).toJSON(), [initialData.Entities]);
+function BeaversMap({ saveData }: State) {
+  const beavers = useMemo(() => lodash(saveData.Entities)
+    .filter((_) => ["BeaverAdult", "BeaverChild"].includes(_.TemplateName)).toJSON(), [saveData.Entities]);
 
   return <group>
     {beavers.map((beaver: UnknownEntity) => <Beaver key={beaver.Id} beaver={beaver} />)}
@@ -127,9 +162,9 @@ function meshWithColorFromGeoms(geometries: any[], color: string) {
   return new Mesh(geom, mat);
 }
 
-function PathsMap({ initialData }: { initialData: DemoSave }) {
+function PathsMap({ saveData }: State) {
   const paths = useMemo(() => {
-    const paths = lodash(initialData.Entities)
+    const paths = lodash(saveData.Entities)
       .filter((_) => ["DirtPath"].includes(_.TemplateName))
       .map((_: any) => new PlaneBufferGeometry(1, 1, 1, 1)
         .rotateX(-Math.PI/2)
@@ -141,14 +176,14 @@ function PathsMap({ initialData }: { initialData: DemoSave }) {
       );
 
     return meshWithColorFromGeoms(paths.toJSON(), "#BCAAA4");
-  }, [initialData.Entities]);
+  }, [saveData.Entities]);
 
   return <primitive object={paths} />;
 }
 
-function TreesMap({ initialData }: { initialData: DemoSave }) {
+function TreesMap({ saveData }: State) {
   const {greenTrees, brownTrees} = useMemo(() => {
-    const trees = lodash(initialData.Entities)
+    const trees = lodash(saveData.Entities)
       .filter((_) => ["Maple", "Pine"].includes(_.TemplateName))
       .map((_: any) => ({
         entity: _,
@@ -166,7 +201,7 @@ function TreesMap({ initialData }: { initialData: DemoSave }) {
       trees.filter(_ => _.dry || _.dead).map(createTreeGeom).toJSON(), "#5D4037");
 
     return {greenTrees, brownTrees}
-  }, [initialData.Entities]);
+  }, [saveData.Entities]);
 
   return <group>
     <primitive object={greenTrees} />
@@ -174,51 +209,41 @@ function TreesMap({ initialData }: { initialData: DemoSave }) {
   </group>;
 }
 
-function SlowBoxesWaterMap({initialData}: {initialData: DemoSave}) {
-  const {Singletons} = initialData;
+function SlowBoxesWaterMap({mapData}: State) {
+  const {i2x, i2y, heightMap, waterDepthMap} = mapData;
+
   const mesh = useMemo(() => {
-    const mapSizeY = Singletons.MapSize.Size.Y;
+    const geoms = lodash(waterDepthMap).reduce((acc, y, i) => {
+      if (y > 0) {
+        acc.push(new PlaneBufferGeometry(1, 1, 1, 1)
+          .rotateX(-Math.PI/2)
+          .translate(i2x(i), y + heightMap[i], i2y(i)));
+      }
+      return acc;
+    }, [] as BufferGeometry[]);
 
-    const heights = Singletons.TerrainMap.Heights.Array.split(" ").map(_ => parseInt(_, 10));
-
-    const geoms = lodash(Singletons.WaterMap.WaterDepths.Array)
-      .split(" ")
-      .map((y) => parseFloat(y))
-      .map((y, i) => y > 0 ? y + heights[i] : 0)
-      .map((y, i) => y > 0 ? ({ x: i % mapSizeY, y, z: Math.floor(i / mapSizeY) }) : null)
-      .compact()
-      .map(({ x, y, z}) => new PlaneBufferGeometry(1, 1, 1, 1).rotateX(-Math.PI/2).translate(x, y, z))
-      .toJSON()
-
-    const geom = BufferGeometryUtils.mergeBufferGeometries(geoms)
-    const mat = new MeshStandardMaterial({ color: "#0044cc" });
-    return new Mesh(geom, mat);
-  }, [Singletons.TerrainMap.Heights.Array, Singletons.WaterMap.WaterDepths.Array, Singletons.MapSize.Size.Y])
+    return meshWithColorFromGeoms(geoms, "#0044cc");
+  }, [i2x, i2y, heightMap, waterDepthMap])
 
   return <primitive object={mesh} />;
 }
 
-function SlowBoxesHeightMap({initialData}: {initialData: DemoSave}) {
-  const {Singletons} = initialData;
-
+function SlowBoxesHeightMap({mapData}: State) {
+  const {heightMap, moistureMap, i2x, i2y} = mapData;
   const {wetLand, dryLand} = useMemo(() => {
-    const mapSizeY = Singletons.MapSize.Size.Y;
-    const wetLevels = Singletons.SoilMoistureSimulator.MoistureLevels.Array.split(" ").map(_ => parseFloat(_));
-
-    const allBoxes = lodash(Singletons.TerrainMap.Heights.Array)
-      .split(" ")
-      .map(_ => parseInt(_, 10))
-      .map((y, i) => ({ x: i % mapSizeY, y, z: Math.floor(i / mapSizeY) }))
-      .map(({ x, y, z}) => new BoxGeometry(1, y, 1, 1, 1, 1).translate(x, y / 2, z));
-
-    const wetBoxes = allBoxes.filter((_, i) => wetLevels[i] > 0).toJSON();
-    const dryBoxes = allBoxes.filter((_, i) => wetLevels[i] === 0).toJSON();
+    const {wetBoxes, dryBoxes} = lodash(heightMap)
+      .reduce((acc, height, index) => {
+        const isWet = moistureMap[index] > 0;
+        const box = new BoxBufferGeometry(1, height, 1, 1, 1, 1).translate(i2x(index), height / 2, i2y(index));
+        (isWet ? acc.wetBoxes : acc.dryBoxes).push(box);
+        return acc;
+      }, { wetBoxes: [] as BufferGeometry[], dryBoxes: [] as BufferGeometry[] })
 
     return {
       wetLand: meshWithColorFromGeoms(wetBoxes, "#8BC34A"),
       dryLand: meshWithColorFromGeoms(dryBoxes, "#795548")
     }
-  }, [Singletons.TerrainMap.Heights.Array, Singletons.MapSize.Size.Y, Singletons.SoilMoistureSimulator.MoistureLevels.Array])
+  }, [i2x, i2y, heightMap, moistureMap])
 
   return <group>
     <primitive object={wetLand} />
