5ff0e779c705d6a1931503243b15e619c0e08e32
Toby Hinloopen <hinlomedia@gmail.com>
2021-09-12 19:20:11 +0200

    Add needs

--- src/plugins/MapPlugin.tsx
+++ src/plugins/MapPlugin.tsx
@@ -1,13 +1,14 @@
-import { DemoSave, UnknownEntity } from "../DemoSave";
+import { BeaverAdultEntity, DemoSave, UnknownEntity } from "../DemoSave";
 import { IEditorPlugin } from "../IEditorPlugin";
 import { Canvas } from '@react-three/fiber'
-import { useMemo, useState } from "react";
-import lodash from "lodash";
+import { FormEvent, useMemo, useState } from "react";
+import lodash, { get, set } from "lodash";
 import { MapControls } from "@react-three/drei";
 import './MapPlugin.scss';
 import { Navbar } from "../Navbar";
 import { BoxBufferGeometry, BufferGeometry, ConeBufferGeometry, Mesh, MeshStandardMaterial, PlaneBufferGeometry } from "three";
 import { EffectComposer, SSAO as _SSAO } from "@react-three/postprocessing";
+import { deepCopy } from "../deepCopy";
 
 const SSAO = _SSAO as any;
 const { BlendFunction } = require("postprocessing") as any;
@@ -16,6 +17,13 @@ const BufferGeometryUtils = require('three/examples/jsm/utils/BufferGeometryUtil
 interface State {
   saveData: DemoSave;
   mapData: MapData;
+  entityData: EntityData;
+}
+
+interface MutableState extends State {
+  setEntity: (entity: UnknownEntity) => void;
+  selectEntityId: (id: string|null) => void;
+  selectedEntity: UnknownEntity|null;
 }
 
 interface MapData {
@@ -28,6 +36,43 @@ interface MapData {
   moistureMap: Float32Array;
   mapSizeX: number;
   mapSizeY: number;
+}
+
+interface EntityData {
+  deleteIds: string[];
+  updateIds: string[];
+  entitiesByIds: Record<string, UnknownEntity>;
+  entitiesIdsByTemplate: Record<string, string[]>;
+}
+
+const EDITABLE_ENTITIES = ["BeaverAdult", "BeaverChild", "Maple", "Pine", "Birch", "DirtPath"];
+
+const useEntitiesOfTypes = (entityData: EntityData, templateIds: string[]) => {
+  const {entitiesIdsByTemplate, entitiesByIds} = entityData;
+  return useMemo(() => lodash(templateIds)
+    .map(_ => entitiesIdsByTemplate[_])
+    .flatten()
+    .map((id) => entitiesByIds[id])
+    .compact()
+    .toJSON(), [templateIds, entitiesIdsByTemplate, entitiesByIds]);
+}
+
+function readEntityData(saveData: DemoSave) {
+  return lodash(saveData.Entities)
+    .filter(_ => EDITABLE_ENTITIES.includes(_.TemplateName))
+    .reduce((acc, entity) => {
+      acc.entitiesByIds[entity.Id] = entity;
+      if (!acc.entitiesIdsByTemplate[entity.TemplateName]) {
+        acc.entitiesIdsByTemplate[entity.TemplateName] = [];
+      }
+      acc.entitiesIdsByTemplate[entity.TemplateName].push(entity.Id);
+      return acc;
+    }, {
+      deleteIds: [],
+      updateIds: [],
+      entitiesByIds: {},
+      entitiesIdsByTemplate: {}
+    } as EntityData)
 }
 
 function readMapData(saveData: DemoSave) {
@@ -58,6 +103,7 @@ export const MapPlugin: IEditorPlugin<State, State> = {
 
   read: (saveData) => ({
     mapData: readMapData(saveData),
+    entityData: readEntityData(saveData),
     saveData
   }),
 
@@ -67,15 +113,42 @@ export const MapPlugin: IEditorPlugin<State, State> = {
     A 3D Map
   </div>,
 
-  Editor: ({ initialData: state, onSubmit, onClose }) => {
-    const {Singletons} = state.saveData;
-    const mapSizeX = Singletons.MapSize.Size.X;
-    const mapSizeY = Singletons.MapSize.Size.Y;
+  Editor: ({ initialData, onSubmit, onClose }) => {
+    const [state, setState] = useState(initialData);
+    const [selectedEntityId, selectEntityId] = useState<string|null>(null);
+    const selectedEntity = (selectedEntityId && state.entityData.entitiesByIds[selectedEntityId]) || null;
+    const {mapSizeX, mapSizeY} = state.mapData;
+
+    const setEntity = (entity: UnknownEntity) => {
+      const oldEntity: UnknownEntity|undefined = state.entityData.entitiesByIds[entity.Id];
+      const newState = {
+        ...state,
+        entityData: {
+          ...state.entityData,
+          entitiesByIds: {
+            ...state.entityData.entitiesByIds,
+            [entity.Id]: entity,
+          },
+        }
+      }
+
+      if (!oldEntity || oldEntity.TemplateName !== entity.TemplateName) {
+        newState.entityData.entitiesIdsByTemplate = {
+          ...newState.entityData.entitiesIdsByTemplate,
+          [oldEntity.TemplateName]: (newState.entityData.entitiesIdsByTemplate[oldEntity.TemplateName] || [])
+            .filter(_ => !oldEntity || (oldEntity && _ !== oldEntity.Id)),
+          [entity.TemplateName]: [...newState.entityData.entitiesIdsByTemplate[entity.TemplateName], entity.Id]
+        };
+      }
+
+      setState(newState);
+    }
 
     return <div className="Map__Editor">
       <Navbar onHome={onClose} />
+      <Gui {...state} selectEntityId={selectEntityId} selectedEntity={selectedEntity} setEntity={setEntity} />
 
-      <Canvas camera={{position: [32, 64, -64]}}>
+      <Canvas className="Map__Canvas" camera={{position: [32, 64, -64]}}>
         <EffectComposer>
           <SSAO
             blendFunction={BlendFunction.MULTIPLY}
@@ -93,7 +166,7 @@ export const MapPlugin: IEditorPlugin<State, State> = {
             <SlowBoxesWaterMap {...state} />
             <TreesMap {...state} />
             <PathsMap {...state} />
-            <BeaversMap {...state} />
+            <BeaversMap {...state} selectEntityId={selectEntityId} selectedEntity={selectedEntity} setEntity={setEntity} />
           </group>
         </group>
         <MapControls />
@@ -102,29 +175,64 @@ export const MapPlugin: IEditorPlugin<State, State> = {
   }
 }
 
-function BeaversMap({ saveData }: State) {
-  const beavers = useMemo(() => lodash(saveData.Entities)
-    .filter((_) => ["BeaverAdult", "BeaverChild"].includes(_.TemplateName)).toJSON(), [saveData.Entities]);
+function Gui(state: MutableState) {
+  if (!state.selectedEntity) {
+    return null;
+  }
+
+  return <div className="Map__Gui">
+    <div className="Map__Gui__Right p-4">
+      <div className="card">
+        <div className="card-body">
+          <h4 className="card-title">{state.selectedEntity.TemplateName}</h4>
+          <BeaverForm {...state} />
+        </div>
+      </div>
+    </div>
+  </div>;
+}
+
+function BeaverForm({ selectedEntity, selectEntityId, setEntity }: MutableState) {
+  const [beaver, setBeaver] = useState<BeaverAdultEntity>(selectedEntity as any);
+
+  const getValue = (path: (string|number)[]) => get(beaver, path);
+  const setValue = (path: (string|number)[], format: (val: string) => any = (x) => x) => (event: FormEvent) => setBeaver(set(deepCopy(beaver), path, format((event.target as any).value)))
+
+  return <form onSubmit={(e) => { e.preventDefault(); setEntity(beaver); selectEntityId(null); }}>
+    <div className="mb-3">
+      <label htmlFor="name" className="form-label">Name</label>
+      <input type="text" id="name" className="form-control" value={getValue(["Components", "Beaver", "Name"])} onChange={setValue(["Components", "Beaver", "Name"])} />
+    </div>
+
+    {beaver.Components.NeedManager.Needs.map((need, index) => <div className="mb-3" key={index}>
+      <label htmlFor={"need-" + index} className="form-label">{need.Name}</label>
+      <input type="range" min="0" max="1" step="0.001" id={"need-" + index} className="form-control"
+        value={getValue(["Components", "NeedManager", "Needs", index, "Points"])}
+        onChange={setValue(["Components", "NeedManager", "Needs", index, "Points"], (val) => parseFloat(val))} />
+    </div>)}
+
+    <div className="mb-3">
+      <button type="submit" className="btn btn-primary">Save</button>
+      {" "}
+      <button type="button" onClick={() => selectEntityId(null)} className="btn btn-light">Discard</button>
+    </div>
+  </form>
+}
+
+function BeaversMap({ entityData, selectEntityId, selectedEntity }: MutableState) {
+  const beavers = useEntitiesOfTypes(entityData, ["BeaverAdult", "BeaverChild"])
 
   return <group>
-    {beavers.map((beaver: UnknownEntity) => <Beaver key={beaver.Id} beaver={beaver} />)}
+    {beavers.map((beaver) => <Beaver selected={selectedEntity === beaver} key={beaver.Id} beaver={beaver} selectEntityId={selectEntityId} />)}
   </group>;
 }
 
-function Beaver({ beaver }: { beaver: UnknownEntity }) {
+function Beaver({ beaver, selectEntityId, selected }: { selected: boolean, beaver: UnknownEntity, selectEntityId: (id: string) => void }) {
   const [isHover, setIsHover] = useState(false);
 
-  const onClick = () => {
-    console.log(beaver);
-  }
-
-  const onPointerEnter = () => {
-    setIsHover(true);
-  }
-
-  const onPointerLeave = () => {
-    setIsHover(false);
-  }
+  const onClick = () => { selectEntityId(beaver.Id); }
+  const onPointerEnter = () => { setIsHover(true); }
+  const onPointerLeave = () => { setIsHover(false); }
 
   const pos = (beaver.Components as any).Beaver.Position;
   const isAdult = beaver.TemplateName === "BeaverAdult";
@@ -132,11 +240,11 @@ function Beaver({ beaver }: { beaver: UnknownEntity }) {
   const y: number = pos.Y + 0.1 + (isAdult ? 0.5 : 0.3);
   const z: number = pos.Z;
   return <mesh onPointerEnter={onPointerEnter} onPointerLeave={onPointerLeave} onClick={onClick} key={beaver.Id} position={[x, y, z]}>
-    <meshStandardMaterial color={isHover ? "#FF8A65" : "#E64A19"} />
+    <meshStandardMaterial color={selected ? "#651FFF" : (isHover ? "#FF8A65" : "#E64A19")} />
     <cylinderBufferGeometry args={[
-      isHover ? 0.6 : 0.4,
-      isHover ? 0.6 : 0.4,
-      (beaver.TemplateName === "BeaverAdult" ? 1.0 : 0.6) * (isHover ? 1.2 : 1.0),
+      (isHover || selected) ? 0.6 : 0.4,
+      (isHover || selected) ? 0.6 : 0.4,
+      (beaver.TemplateName === "BeaverAdult" ? 1.0 : 0.6) * (isHover || selected ? 1.2 : 1.0),
       8.0,
       1.0,
     ]} />
@@ -157,35 +265,34 @@ function createTreeGeom({ dry, dead, adult, x, y, z }: {
 }
 
 function meshWithColorFromGeoms(geometries: any[], color: string) {
+  if (geometries.length === 0) {
+    return new Mesh();
+  }
   const geom = BufferGeometryUtils.mergeBufferGeometries(geometries)
   const mat = new MeshStandardMaterial({ color });
   return new Mesh(geom, mat);
 }
 
-function PathsMap({ saveData }: State) {
-  const paths = useMemo(() => {
-    const paths = lodash(saveData.Entities)
-      .filter((_) => ["DirtPath"].includes(_.TemplateName))
-      .map((_: any) => new PlaneBufferGeometry(1, 1, 1, 1)
-        .rotateX(-Math.PI/2)
-        .translate(
-          _.Components.BlockObject.Coordinates.X,
-          _.Components.BlockObject.Coordinates.Z + 0.1,
-          _.Components.BlockObject.Coordinates.Y
-        ),
-      );
+function PathsMap({ entityData }: State) {
+  const paths = useEntitiesOfTypes(entityData, ["DirtPath"]);
+  const geom = useMemo(() => meshWithColorFromGeoms(paths
+    .map((_: any) => new PlaneBufferGeometry(1, 1, 1, 1)
+      .rotateX(-Math.PI/2)
+      .translate(
+        _.Components.BlockObject.Coordinates.X,
+        _.Components.BlockObject.Coordinates.Z + 0.1,
+        _.Components.BlockObject.Coordinates.Y
+      ),
+    ), "#BCAAA4"), [paths]);
 
-    return meshWithColorFromGeoms(paths.toJSON(), "#BCAAA4");
-  }, [saveData.Entities]);
-
-  return <primitive object={paths} />;
+  return <primitive object={geom} />;
 }
 
-function TreesMap({ saveData }: State) {
+function TreesMap({ entityData }: State) {
+  const treeEntities = useEntitiesOfTypes(entityData, ["Pine", "Maple", "Birch"]);
+
   const {greenTrees, brownTrees} = useMemo(() => {
-    const trees = lodash(saveData.Entities)
-      .filter((_) => ["Maple", "Pine"].includes(_.TemplateName))
-      .map((_: any) => ({
+    const trees = treeEntities.map((_: any) => ({
         entity: _,
         dry: _.Components.WateredObject.IsDry as boolean,
         dead: _.Components.LivingNaturalResource.IsDead as boolean,
@@ -196,12 +303,12 @@ function TreesMap({ saveData }: State) {
       }));
 
     const greenTrees = meshWithColorFromGeoms(
-      trees.filter(_ => !(_.dry || _.dead)).map(createTreeGeom).toJSON(), "#388E3C");
+      trees.filter(_ => !(_.dry || _.dead)).map(createTreeGeom), "#388E3C");
     const brownTrees = meshWithColorFromGeoms(
-      trees.filter(_ => _.dry || _.dead).map(createTreeGeom).toJSON(), "#5D4037");
+      trees.filter(_ => _.dry || _.dead).map(createTreeGeom), "#5D4037");
 
     return {greenTrees, brownTrees}
-  }, [saveData.Entities]);
+  }, [treeEntities]);
 
   return <group>
     <primitive object={greenTrees} />
@@ -209,7 +316,7 @@ function TreesMap({ saveData }: State) {
   </group>;
 }
 
-function SlowBoxesWaterMap({mapData}: State) {
+function SlowBoxesWaterMap({ mapData }: State) {
   const {i2x, i2y, heightMap, waterDepthMap} = mapData;
 
   const mesh = useMemo(() => {
@@ -228,7 +335,7 @@ function SlowBoxesWaterMap({mapData}: State) {
   return <primitive object={mesh} />;
 }
 
-function SlowBoxesHeightMap({mapData}: State) {
+function SlowBoxesHeightMap({ mapData }: State) {
   const {heightMap, moistureMap, i2x, i2y} = mapData;
   const {wetLand, dryLand} = useMemo(() => {
     const {wetBoxes, dryBoxes} = lodash(heightMap)
